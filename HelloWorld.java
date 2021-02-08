fb3955af74e0941a92ab19eaa8a90c6a0218f826
Eman Idris <eidris00@email.cpcc.edu>
2021-02-08 14:02:13 -0500

    Revert "Modified HelloWorld Program"
    This reverts commit bd9454228971f6de802fade15a51b9a546414232.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,5 @@
 public class HelloWorld {
    public static void main (String[] args) {
       System.out.println("Hello World!");
-      System.out.println("Program modified, but able " +
-                         "revert to original version using " +
-                         "GitHub Desktop." );
    }
 }
